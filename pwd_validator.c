93a244e5c3109afef5154b76f92fe7dc58303c99
Aviksh <59430239+Aviksh0215@users.noreply.github.com>
2020-01-18 20:45:25 +0530

    Add files via upload

---
+++ pwd_validator.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+#include<string.h>
+
+int main()
+
+{
+
+    int a,cap = 0,num = 0,spec = 0;
+
+    char pwd[20];
+
+    sanrush:
+
+    printf("Enter the username");
+
+    gets(pwd);
+
+    for(a=0;a<=strlen(pwd);a++)
+
+    {
+
+    if(pwd[a]>=65 && pwd[a]<=90)
+
+    {
+
+        cap++;
+
+    }
+
+    else if(pwd[a]>=48 && pwd[a]<=57)
+
+    {
+
+        num++;
+
+    }
+
+    else if(pwd[a]>=33 && pwd[a]<=47 || pwd[a]>=58 && pwd[a]<=64 || pwd[a]>=123 && pwd[a]<=126)
+
+    {
+
+        spec++;
+
+    }
+
+    }
+
+    if(cap ==0)
+
+    {
+
+        printf("Plz enter the capital charachter\n");
+
+    }
+
+    if(num == 0)
+
+    {
+
+        printf("Plz enter the atlest one number\n");
+
+    }
+
+    if(spec == 0)
+
+    {
+
+        printf("plz enter the atlest one special\n");
+
+    }
+
+    if(cap==0 && num == 0 && spec == 0)
+
+    {
+
+        printf("Plz Enter valid passowrd");
+
+    }
+
+    if(cap==0 || num == 0 || spec == 0)
+
+    goto sanrush;
+
+    
+return 0;
+
+}
